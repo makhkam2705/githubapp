080fd0d64e1428eb3308b6db7f793c1cb8bd375c
Makhkam <makhkam00@go.dev>
2021-09-26 19:23:40 +0500

    довавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,35 @@
+package types
+
+// Money представляет собой денежную сумму в минимальных единацах
+type Money int64
+
+// Currency представляет кол валюты
+type Currency string
+
+// Код валяты
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// PAN представляет номер карты
+type PAN string
+
+// Card представляет информацию о платёжной карте
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money // использовать Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+	MinBalance Money
+}
+
+// Payment представляет информацию о платеже.
+type Payment struct {
+	ID     int
+	Amount Money
+}
